ecdc63f8f99bb73a9ba8ceda68839617ed1f27a2
InohPark <inohzzang@gmail.com>
2022-07-22 18:57:42 +0900

    변수명 사전 비교

--- PythonElegantChecker/constants.py
+++ PythonElegantChecker/constants.py
@@ -2,8 +2,10 @@ import re
 
 
 # x, y, z, w, i, j, k, l are allowed in variable names or indices
-REGEX_SINGLE_VAR = re.compile(r'([A-H]|[M-V]|[a-h]|[m-v])')
-REGEX_DOUBLE_VAR = re.compile(r'([A-Z]|[a-z]){2}')
+REGEX_VAR_NAME_ERROR = [
+                          re.compile(r'([A-H]|[M-V]|[a-h]|[m-v])'),
+                          re.compile(r'([A-Z]|[a-z]){2}')
+]
 
 # Regex for camelCase to snake_case.
 REGEX_CAMEL_TO_SNAKE1 = re.compile(r'([A-Z]+)([A-Z][a-z])')
--- PythonElegantChecker/main.py
+++ PythonElegantChecker/main.py
@@ -5,23 +5,34 @@ import utils
 def main():
     instance = elegance.Elegance()
 
-    print('file check:')
+    # check for PEP8 violations
+    print('\nfile check:')
     files = utils.get_files_by_dir()
     reports = instance.get_PEP8_metrics(files)
-    violation_counts = [len(report) for report in reports]
-    print(violation_counts)
+    PEP8_violation_counts = [len(report) for report in reports]
+    print('PEP8 violations:', reports)
+    print('PEP8 violation counts:', PEP8_violation_counts)
 
-    print('code check:')
+    # check for code metrics
+    print('\ncode check:')
     codes = utils.get_codes(files)
-    depths = elegance.get_cyclomatic_metrics(codes)
+    depths = instance.get_cyclomatic_metrics(codes)
     max_depths = [max(depth) if len(depth) > 0 else 0 for depth in depths]
-    raw_metrics = elegance.get_raw_metrics(codes)
-    mi_metrics = elegance.get_mi_metrics(codes)
+    mi_metrics = instance.get_mi_metrics(codes)
+    # raw_metrics = elegance.get_raw_metrics(codes)
 
     print('depths:', depths)
     print('max depths:', max_depths)
-    print('raw metrics:', raw_metrics)
-    print('mi metrics:', mi_metrics)
+    print('Maintainability Index metrics:', mi_metrics)
+    # print('raw metrics:', raw_metrics)
+
+    # check for variable names
+    print('\nvariable name check:')
+    # test tokens
+    tokens = ['test', 'aaa', 'bbb', 'ccc', 'aaba', 'ab']
+    violations = instance.check_name_correctness(tokens)
+    violation_counts = violations.count(True)
+    print('name violation counts:', violation_counts)
 
 
 if __name__ == '__main__':
