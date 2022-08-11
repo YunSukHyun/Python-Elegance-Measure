529dffd2454d671c3d7693fe00b81ae0d44cc911
YunSukHyun <czcz9207@gmail.com>
2022-08-11 18:40:24 +0900

    fileUploader restore

---
+++ client/src/components/fileUploader/fileUploader.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import {toast} from 'react-toastify'
+import './fileUploader.css';
+function FileUploader() {
+  const [files, setFiles] = useState([]);
+  const [fileInputText, setFileInputText] = useState("form-control noShow");
+  const onInputChange = (e) => {
+    console.log(e.target.files);
+    setFiles(e.target.files);
+    setFileInputText("form-control");
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    for(let i = 0; i < files.length; i++){
+      data.append('file', files[i]);
+    }
+    data.append('file', files);
+    axios.post('//localhost:5000/upload', data)
+    .then((e)=>{
+      toast.success('Upload Success');
+    })
+    .catch((e)=>{
+      toast.error('Upload Error');
+    })
+  }
+  return (
+    <form method="post" action="#" id="#" onSubmit={onSubmit}> 
+      <div className="form-group files">
+        <input
+          type="file"
+          onChange={onInputChange}
+          className={fileInputText}
+          multiple/>
+      </div>
+      <button className="snip1535">Similarity Check</button>
+      <button className="snip1535">Elegance Measure</button>
+    </form>
+  )
+}
+
+export default FileUploader
